074e399664ac6465baea5b22463e8fd8e589ff5c
cernajr <jorcer26@gmail.com>
2025-03-05 15:17:44 -0600

    Agregue nuevos vlistitem  y modifique foto de perfil

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,9 @@
     <v-navigation-drawer expand-on-hover rail>
       <v-list>
         <v-list-item
-          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-          subtitle="sandra_a88@gmailcom"
-          title="Sandra Adams"
+          :prepend-avatar="logoImage"
+          subtitle="Rapido y Eficaz"
+          title="Facturalize"
         ></v-list-item>
       </v-list>
 
@@ -13,19 +13,59 @@
 
       <v-list density="compact" nav>
         <v-list-item
-          prepend-icon="mdi-folder"
-          title="My Files"
-          value="myfiles"
+          prepend-icon="mdi-view-dashboard"
+          title="Dashboard"
+          value="dashboard"
         ></v-list-item>
         <v-list-item
           prepend-icon="mdi-account-multiple"
-          title="Shared with me"
-          value="shared"
+          title="Clientes"
+          value="clientes"
         ></v-list-item>
         <v-list-item
-          prepend-icon="mdi-star"
-          title="Starred"
-          value="starred"
+          prepend-icon="mdi-package-variant-closed"
+          title="Productos"
+          value="productos"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-file-document-outline"
+          title="Facturas"
+          value="facturas"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-file-document-edit-outline"
+          title="Cotizaciones"
+          value="cotizaciones"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-truck"
+          title="Proveedores"
+          value="proveedores"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-cart"
+          title="Compras"
+          value="compras"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-chart-bar"
+          title="Reportes"
+          value="reportes"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-cog"
+          title="Configuración"
+          value="configuracion"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-help-circle"
+          title="Ayuda"
+          value="ayuda"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-logout"
+          title="Cerrar Sesión"
+          value="logout"
         ></v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -52,6 +92,8 @@
 <script setup>
 import { useTheme } from "vuetify";
 import { onMounted } from "vue";
+import { ref } from "vue";
+import logoImage from "@/assets/logo.png";
 
 const theme = useTheme();
 
